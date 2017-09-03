5ce87dbb3bd28b15b110fe031b2323018f7f1c15
mr-scneider <rileysndr@gmail.com>
2017-09-03 16:29:18 -0500

    Connection to SQL server initalized.

--- html/connection.php
+++ html/connection.php
@@ -1,20 +1,16 @@
+<?php
+  $host = "localhost";
+  $username = "root";
+  $password = "raisehand";
+  $cdname = "topics";
+  //echo "About to try...<br>";
+  $mysqlconn = new mysqli("localhost", "root", "raisehand") or die('Error connecting to server');
+?>
+
 <html>
   <body>
     <p>
-    <?php
-      $host = "localhost";
-      $username = "root";
-      $password = "raisehand";
-      $cdname = "topics";
-      echo "About to try...<br>";
-      $mysqlconn = new mysqli(host, username, password);
-      if($mysqlconn->connect_error) {
-        echo "Awe damn, not good.";
-      }
-      else {
-        echo "Hey, we out here!";
-      }
-    ?>
+    "We are connected"
   </p>
   </body>
 </html>
