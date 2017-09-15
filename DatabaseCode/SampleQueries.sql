39faf0ab8e11e5ff7529258587e9bc0e713a1c8a
Stephanie Engelhardt <sae1@iastate.edu>
2017-09-15 08:16:51 -0500

    Added order by conditions for the threads and the replies to list in order of points given

--- DatabaseCode/SampleQueries.sql
+++ DatabaseCode/SampleQueries.sql
@@ -2,9 +2,10 @@
 Written by Stephanie Engelhardt on 09/15/2017 **/
 
 /* This query selects all of the threads and their replies and joins them together
-to allow access to both of them. */
+to allow access to both of them. Orders them in order of most upped thread, (then by most upped reply)*/
 SELECT thread.*, reply.* FROM threads thread
-LEFT OUTER JOIN replies reply ON thread.ID=reply.thread_id;
+LEFT OUTER JOIN replies reply ON thread.ID=reply.thread_id
+ORDER BY thread.points, reply.points;
 
 /* This query selects all of the flagged posts from the same university as the admin */
 SELECT * FROM replies reply
@@ -16,3 +17,9 @@ SELECT * FROM replies reply
 /*This query selects all of the threads (aka questions) that a user has created*/
 SELECT * FROM threads
 WHERE threads.owner_id= @sys_user.ID;
+
+/* Shows all of the topics from the class that the user is in */
+SELECT * FROM topics
+WHERE topics.class_id= @sys_user.class_id;
+
+
