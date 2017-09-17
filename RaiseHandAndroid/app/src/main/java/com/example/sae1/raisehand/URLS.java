62bd13246a3194d89ee08ab9c4aa49db7d79c178
Stephanie Engelhardt <sae1@iastate.edu>
2017-09-17 18:30:05 -0500

    Adding more files to help with communicating to the server

---
+++ RaiseHandAndroid/app/src/main/java/com/example/sae1/raisehand/URLS.java
@@ -0,0 +1,12 @@
+package com.example.sae1.raisehand;
+
+/**
+ * Created by sae1 on 9/17/17.
+ */
+
+public class URLS {
+    //TODO: Add root URL
+    private static final String ROOT_URL = " ";
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+}
