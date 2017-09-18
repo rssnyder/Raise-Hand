c47341ab9d2856b79feb3740a541ba47ac7659a3
Joel Schwartz <joel2@iastate.edu>
2017-09-17 21:51:27 -0500

    added teacher classes page

--- RaiseHandAndroid/app/src/main/java/com/example/sae1/raisehand/TeacherHomePage.java
+++ RaiseHandAndroid/app/src/main/java/com/example/sae1/raisehand/TeacherHomePage.java
@@ -1,5 +1,8 @@
 package com.example.sae1.raisehand;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +14,7 @@ public class TeacherHomePage extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-
+    private NavigationView nv;
     private Toolbar mToolbar;
 
     @Override
@@ -29,6 +32,19 @@ public class TeacherHomePage extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView) findViewById(R.id.nv1);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case (R.id.nav_classes):
+                        Intent teacherClasses = new Intent(getApplicationContext(),TeacherClasses.class);
+                        startActivity(teacherClasses);
+                }
+                return true;
+            }
+        });
     }
 
     @Override
