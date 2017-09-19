858c066ff2ce8f258f46c709993bed175b8145af
jaggarwal96 <jaggarwal@wisc.edu>
2017-09-19 08:56:45 -0500

    Skeleton somewhat started

--- RaiseHandAndroid/app/src/main/java/utils/LoginActivity.java
+++ RaiseHandAndroid/app/src/main/java/utils/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.content.Intent;
 import android.widget.TextView;
 import com.android.volley.Request.Method;
 import com.android.volley.Response;
@@ -43,7 +44,9 @@ public class LoginActivity extends Activity {
         buttonLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, com.example.sae1.raisehand.student_home_page.class);
                 userLogin();
+
             }
         });
     }
