edf37faf9bff6ab1ece14a76690b4620a454c140
sae1 <sae1@iastate.edu>
2017-09-21 15:22:23 -0500

    Update signin.php

--- signin.php
+++ signin.php
@@ -67,9 +67,9 @@
         die("Going to admin panel");
       }
       //Send user to their homepage
-     
+     return $username;
       header("Location: home.php");
-      return $username;
+      
       
 
     }
