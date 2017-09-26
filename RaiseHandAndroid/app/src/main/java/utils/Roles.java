482ebc42661f3954df187249e35d96ada941c334
Joel Schwartz <joel2@iastate.edu>
2017-09-26 18:33:34 -0500

    goes to the correct page depending on your role

---
+++ RaiseHandAndroid/app/src/main/java/utils/Roles.java
@@ -0,0 +1,24 @@
+package utils;
+
+public enum Roles {
+
+    ADMIN("1"),
+    TEACHER("2"),
+    TA("3"),
+    STUDENT("4");
+
+    private final String role;
+
+    private Roles(String r){
+        role = r;
+    }
+
+    public boolean equalsName(String otherRole) {
+        return role.equals(otherRole);
+    }
+
+    public String toString() {
+        return this.role;
+    }
+
+}
