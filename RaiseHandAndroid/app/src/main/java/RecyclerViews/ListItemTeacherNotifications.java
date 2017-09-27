e6b135c1ebca6f362c9d093a622c179ab36f999f
Joel Schwartz <joel2@iastate.edu>
2017-09-27 13:19:02 -0500

    added test RecyclerView to TeacherNotifications activity

---
+++ RaiseHandAndroid/app/src/main/java/RecyclerViews/ListItemTeacherNotifications.java
@@ -0,0 +1,20 @@
+package RecyclerViews;
+
+public class ListItemTeacherNotifications {
+
+    private String head;
+    private String desc;
+
+    public ListItemTeacherNotifications(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
