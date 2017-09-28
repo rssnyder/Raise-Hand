9d74dabb26e4fde204a416799336ca0b6e9ed3be
Stephanie Engelhardt <sae1@iastate.edu>
2017-09-27 20:20:14 -0500

    Updated user class table to follow protcols

--- DatabaseCode/Class_user_relationship_table.sql
+++ DatabaseCode/Class_user_relationship_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE UserClasses (
-relationship int NOT NULL auto_increment,
+CREATE TABLE userClasses (
+ID int NOT NULL auto_increment,
+relationship int NOT NULL,
 user_id int not null,
 class_id int not null,
-PRIMARY KEY (relationship),
+PRIMARY KEY (ID),
 FOREIGN KEY (class_id) references classes(ID),
 FOREIGN KEY (user_id) references users(ID)
 );
