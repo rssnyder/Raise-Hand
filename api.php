cdeb2c6ac675be1b8a39e572833c295bafe0610d
sae1 <sae1@iastate.edu>
2017-09-28 10:45:11 -0500

    Update api.php

--- api.php
+++ api.php
@@ -40,7 +40,7 @@
 	}
 	if($user['logged_in']){
 	    $tempID= $response['ID'];
-	    $stmt = "SELECT class_id FROM UserClasses WHERE user_id = '$tempID' ";
+	    $stmt = "SELECT class_id FROM userClasses WHERE user_id = '$tempID' ";
 	    $stmt = $db->query($stmt) or die($db->error);
 	    $res = $stmt->fetch_assoc();
 	    if(empty($res)){
