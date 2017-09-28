78cd1fbf36a0a9f9aa8fdc0f8b9900bbb7d29134
mr-scneider <rssnyder@iastate.edu>
2017-09-28 15:59:40 -0500

    testing

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <html>
   <body>
     <center>
-      <form id="recover-form" action="utilities/deploy.php" method="post">
+      <form id="recover-form" action="realDeploy.php" method="post">
         <input name="signup" type="submit" value="Deploy">
       </form>
     </center>
