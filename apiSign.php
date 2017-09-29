e2becf0ab0cc1177c2d7601e5ae27e044387f411
sae1 <sae1@iastate.edu>
2017-09-29 10:52:13 -0500

    Add new file, using the sign in php I made to start working on login php

---
+++ apiSign.php
@@ -0,0 +1,52 @@
+<?php
+
+	//require_once 'sql.php';
+	$host="mysql.cs.iastate.edu";
+	$port=3306;
+	$socket="";
+	$user="dbu309sab3";
+	$password="SD0wFGqd";
+	$dbname="db309sab3";
+	//Connect to database
+	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
+	$username = $_GET['username'];
+	$pass = $_GET['pass'];
+	$stmt = "SELECT * FROM users WHERE username = '$username'";
+	$stmt = $db->query($stmt) or die($db->error);
+	$response = $stmt->fetch_assoc();
+	$password = $response['pass'];
+
+	//If correct password
+	if(password_verify($pass, $password)) {
+		$user = array(
+		    'logged_in'=>"true",
+		    'id'=>$response['ID'],
+		    'role_id'=> $response['role_id'],
+		    'username'=>$response['username'],
+		    'first_name'=>$response['first_name'],
+		    'last_name'=>$response['last_name']
+		);
+		$response['error'] = false;
+		$response['message'] = 'Login successfull';
+		$response['user'] = $user;
+	}
+	else{
+	    $user=array(
+	        'logged_in'=>"false"
+	   );
+		$response['error'] = false;
+		$response['message'] = 'Invalid username or password';
+		$res['class_id']="0";
+	}
+	if($user['logged_in']){
+	    $tempID= $response['ID'];
+	    $stmt = "SELECT class_id FROM userClasses WHERE user_id = '$tempID' ";
+	    $stmt = $db->query($stmt) or die($db->error);
+	    $res = $stmt->fetch_assoc();
+	    if(empty($res)){
+	        $res['class_id']="0";
+	    }
+	}
+	$final = array_merge($user, $res);
+	die(json_encode($final));
+?>
