7c6a2ade18fadd4631d566a84b0787ffe20350d7
Joel Schwartz <joel2@iastate.edu>
2017-10-04 14:12:36 -0500

    dummy notifications to teacher classes

--- RaiseHandAndroid/app/src/main/java/app/TeacherClasses.java
+++ RaiseHandAndroid/app/src/main/java/app/TeacherClasses.java
@@ -4,14 +4,31 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import com.example.sae1.raisehand.R;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import RecyclerViews.ListItemTeacherClasses;
+import RecyclerViews.MyAdapterClasses;
+import RecyclerViews.MyAdapterNotifications;
+
 public class TeacherClasses extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItemTeacherClasses> listItems;
+    private Field mDragger;
+
+
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private NavigationView nv;
@@ -22,6 +39,20 @@ public class TeacherClasses extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_classes);
 
+        recyclerView = (RecyclerView) findViewById(R.id.classesRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            ListItemTeacherClasses listItem = new ListItemTeacherClasses("Class " + (i + 1),
+                                                                         "Dummy text. I'm here to notify you!");
+            listItems.add(listItem);
+        }
+        adapter = new MyAdapterClasses(listItems, this);
+
+        recyclerView.setAdapter(adapter);
+
         mToolbar = (Toolbar) findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
 
@@ -30,6 +61,8 @@ public class TeacherClasses extends AppCompatActivity {
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+        slideOutMenu();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -74,5 +107,45 @@ public class TeacherClasses extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void slideOutMenu(){
+
+        try {
+            mDragger = mDrawerLayout.getClass().getDeclaredField(
+                    "mLeftDragger");//mRightDragger for right obviously
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        mDragger.setAccessible(true);
+        ViewDragHelper draggerObj = null;
+        try {
+            draggerObj = (ViewDragHelper) mDragger
+                    .get(mDrawerLayout);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        Field mEdgeSize = null;
+        try {
+            mEdgeSize = draggerObj.getClass().getDeclaredField(
+                    "mEdgeSize");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        mEdgeSize.setAccessible(true);
+        int edge = 0;
+        try {
+            edge = mEdgeSize.getInt(draggerObj);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            mEdgeSize.setInt(draggerObj, edge * 25);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
