290a6fee33943428bcdcefa89c1687e3edf3044c
Joel Schwartz <joel2@iastate.edu>
2017-10-04 16:07:45 -0500

    checks if all sign up fields are filled out

--- RaiseHandAndroid/app/src/main/java/utils/SignupActivity.java
+++ RaiseHandAndroid/app/src/main/java/utils/SignupActivity.java
@@ -71,17 +71,42 @@ public class SignupActivity extends AppCompatActivity {
 
     }
     public void signUp() {
+        boolean isValidSignUp = true;
         final String first = FirstName.getText().toString();
         final String last = LastName.getText().toString();
         final String username = Username.getText().toString();
         final String password = Password.getText().toString();
         final String email = Email.getText().toString();
+
         final String university="Iowa State University";
-        final String email_user= email.substring(0, email.indexOf("@"));
+        String email_user = "";
+        try {
+            email_user = email.substring(0, email.indexOf("@"));
+            isValidSignUp = true;
+        } catch(StringIndexOutOfBoundsException e){
+            Toast.makeText(MainActivity.getInstance(), "Please enter a valid email address", Toast.LENGTH_LONG).show();
+            isValidSignUp = false;
+        }
         final String email_domain=email.substring(email.indexOf("@")+1);
-        //url to be used to get the user information via PHP/ volley
-        String urlSuffix= "?first="+first+"&last="+last+"&pass="+password+"&username="+username+"&university="+university_id+"&emailU="+ email_user+ "&emailD="+ email_domain;
-        String url_final= URLS.URL_REGISTER+urlSuffix;
+
+        boolean allFieldsFilled = !first.isEmpty() && !last.isEmpty() && !username.isEmpty() && !password.isEmpty() && !email.isEmpty();
+
+        if(!allFieldsFilled){
+            isValidSignUp = false;
+            Toast.makeText(MainActivity.getInstance(), "Please fill out all fields", Toast.LENGTH_LONG).show();
+        }
+        if((email_domain.isEmpty() || !email_domain.contains(".")) && allFieldsFilled){
+            Toast.makeText(MainActivity.getInstance(), "Please enter a valid email address", Toast.LENGTH_LONG).show();
+            isValidSignUp = false;
+        }
+
+        if(isValidSignUp) {
+            //url to be used to get the user information via PHP/ volley
+            String urlSuffix =
+                    "?first=" + first + "&last=" + last + "&pass=" + password + "&username=" +
+                    username + "&university=" + university_id + "&emailU=" + email_user +
+                    "&emailD=" + email_domain;
+            String url_final = URLS.URL_REGISTER + urlSuffix;
      /*
         //validating inputs
         if (TextUtils.isEmpty(FirstName)) {
@@ -96,32 +121,32 @@ public class SignupActivity extends AppCompatActivity {
             return;
         }
         showProgressDialog();*/
-        StringRequest req = new StringRequest(Request.Method.GET,url_final,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+            StringRequest req = new StringRequest(Request.Method.GET, url_final,
+                                                  new Response.Listener<String>() {
+                                                      @Override
+                                                      public void onResponse(String response) {
 
-                        Log.d(TAG, response.toString());
-                        String phpResponse=response.toString();
-                        //in the php file, the user information is stored in an array with : as a delimiter between the variable name and actual value
-                        if(phpResponse.contains("User exists")){
-                            Toast.makeText(MainActivity.getInstance(), "User exists, please try again", Toast.LENGTH_LONG).show();
-                        }
-                        else if(phpResponse.contains("Done")){
-                            Toast.makeText(MainActivity.getInstance(), "Success: user created", Toast.LENGTH_LONG).show();
-                        }
-                        else{
-                            Toast.makeText(MainActivity.getInstance(), "Error", Toast.LENGTH_LONG).show();
-                        }
-                    }}, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                                                          Log.d(TAG, response.toString());
+                                                          String phpResponse = response.toString();
+                                                          //in the php file, the user information is stored in an array with : as a delimiter between the variable name and actual value
+                                                          if (phpResponse.contains("User exists")) {
+                                                              Toast.makeText(MainActivity.getInstance(), "User exists, please try again", Toast.LENGTH_LONG).show();
+                                                          } else if (phpResponse.contains("Done")) {
+                                                              Toast.makeText(MainActivity.getInstance(), "Success: user created", Toast.LENGTH_LONG).show();
+                                                          } else {
+                                                              Toast.makeText(MainActivity.getInstance(), "Error", Toast.LENGTH_LONG).show();
+                                                          }
+                                                      }
+                                                  }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(TAG, "Error: " + error.getMessage());
+                }
             }
+            );
+            // Adding request to request queue
+            MainActivity.getInstance().addToRequestQueue(req, tag_string_req);
         }
-        );
-        // Adding request to request queue
-        MainActivity.getInstance().addToRequestQueue(req, tag_string_req);
     }
     public void universityID(String result){
         //need to keep adding universities here
