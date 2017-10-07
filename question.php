fd5af373ddf67aab859516e276b694fadcb58b66
sae1 <sae1@iastate.edu>
2017-10-06 19:14:17 -0500

    Update question.php to return based on a topic id

--- question.php
+++ question.php
@@ -10,17 +10,9 @@
 	//Connect to database
 	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
 	
-	//start to read from the url
-	$class_id=$_GET['classId'];
-	$thread_id = $_GET['threadId'];
-    //TODO: help this
-    //TODO: Add in constraints, probably username and universityID, class ID? thread ID?
-    $question = "SELECT * FROM threads WHERE class_id='$class_id'";
+	$topic_id = $_GET['topicId'];
+    $question = "SELECT title, description, endorsed_user_id, creation, points, user_name FROM threads WHERE topic_id='$topic_id'";
     //Excecute
     $result = $db->query($question) or die($db->error);
 	die($result);
-
-  
-
-
 ?>
