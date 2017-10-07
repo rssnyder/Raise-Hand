3066ea47b62882b4bd1507b40a7c559002e0eb98
sae1 <sae1@iastate.edu>
2017-10-06 19:34:35 -0500

    Update topics.php, accidentally wrote the wrong parameter the first time

--- topics.php
+++ topics.php
@@ -10,7 +10,7 @@
 	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
 	
 	$class_id = $_GET['classId'];
-    $question = "SELECT topic_name, description FROM topics WHERE topic_id='$class_id'";
+    $question = "SELECT topic_name, description FROM topics WHERE class_id='$class_id'";
     //Excecute
     $result = $db->query($question) or die($db->error);
 	die($result);
