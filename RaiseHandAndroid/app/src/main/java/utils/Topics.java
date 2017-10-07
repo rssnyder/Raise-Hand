32416d1ff12d99beec68934c74a161dcdb96178b
Stephanie Engelhardt <sae1@iastate.edu>
2017-10-06 20:29:31 -0500

    Working on parsing the string

--- RaiseHandAndroid/app/src/main/java/utils/Topics.java
+++ RaiseHandAndroid/app/src/main/java/utils/Topics.java
@@ -23,7 +23,10 @@ import app.TeacherNotifications;
  */
 
 public class Topics {
+    //Needed to help track errors with android volley
+    private String tag_string_req= "topics_req";
     private String TAG= Topics.class.getSimpleName();
+
     // description of question
     private String description;
 
@@ -66,6 +69,7 @@ public class Topics {
     public ArrayList<Topics> get_topics(int classID) {
         ArrayList<Topics> t= new ArrayList<Topics>();
         ArrayList<Question> q= new ArrayList<Question>();
+        ArrayList<Reply> replies= new ArrayList<Reply>();
         int existsQuestions;
         int existsTopics;
         String urlSuffix= "?classId="+classID;
@@ -74,90 +78,24 @@ public class Topics {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-
                         Log.d(TAG, response.toString());
                         String phpResponse=response.toString();
-                        //in the php file, the user information is stored in an array with : as a delimiter between the variable name and actual value
-                        String[] seperated=phpResponse.split(":");
+                        //The string can contain multiple parts to indicate when we start reading new information
 
-                        if(seperated[1].contains("true")) {
-                            //concat strings to make it so that the array is properly read from the php response
-                            String reset=seperated[2];
-                            reset=reset.substring(1, reset.indexOf(",")-1);
-                            String unique_id=seperated[3];
-                            unique_id=unique_id.substring(1, unique_id.indexOf(",")-1);
-                            String roleID=seperated[4];
-                            roleID=roleID.substring(1,roleID.indexOf(",")-1);
-                            String usern=seperated[5];
-                            usern=usern.substring(1, usern.indexOf(",")-1);
-                            String first=seperated[6];
-                            first=first.substring(1, first.indexOf(",")-1);
-                            String last=seperated[7];
-                            last=last.substring(1,last.indexOf(",")-1);
-                            String class_ids=seperated[8];
-                            class_ids=class_ids.substring(1, class_ids.length()-2);
-                            //In the php file, if someone is not in a class, I made it return 0 (meaning no classes for this user)
-                            if(class_ids=="0") {
-                                class_ids = "None available";
-                            }
-                            Toast.makeText(MainActivity.getInstance(), "Welcome back, "+first+"!", Toast.LENGTH_LONG).show();
-                            currentUser=new User(reset, unique_id,roleID,usern,first,last,class_ids, true);
+                        //NEWTOPIC indicates the start of a new topic, make a new topic object
 
-                            //store the username on login
-                            SharedPreferences.Editor editor = mPreferences.edit();
-                            editor.putString("reset", reset);
-                            editor.putString("username", usern);
-                            editor.putString("role", roleID);
-                            editor.putString("unique_id", unique_id);
-                            editor.putString("first_name", first);
-                            editor.putString("last_name", last);
-                            //editor.putString("classes",class_ids);
-                            editor.commit();
+                            //NEWQUESTION means the start of the new question within this topic, add to array list
 
-                            //TODO make it go to the student or teacher page depending on what kind of user logged in
-                            //Go to the teacher notification page
-                            if(roleID.equals(Roles.TEACHER.toString())) {
-                                Intent teacherNotifications =
-                                        new Intent(getApplicationContext(), TeacherNotifications.class);
-                                startActivity(teacherNotifications);
-                                finish(); //finsih this activity so you can't press back to go to the login screen after already logging in
-                            }
-
-                        }
-                        else {
-                            Toast.makeText(MainActivity.getInstance(), "Logged In Failed", Toast.LENGTH_LONG).show();
-                        }
-                        hideProgressDialog();
+                                //NEWREPLY means the start of a new reply within this question, add to the question's array list
                     }}, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 VolleyLog.d(TAG, "Error: " + error.getMessage());
-                hideProgressDialog();
             }
         }
         );
         // Adding request to request queue
         MainActivity.getInstance().addToRequestQueue(req, tag_string_req);
-
-        if(existsQuestions==1) {
-            //todo: while loop to go through all of the questions
-            String description, title, ownerID, universityID;
-            int studentRating;
-            Date creationTime;
-            List<Reply> replies = new ArrayList<Reply>();
-            int existReplies;
-            //Return from the database information on whether or not there are replies to this question
-            if (existReplies == 1) {
-                //If there are replies, need to make a method that gets the information on all of the replies, similar to this question method
-            }
-            q.add(new Question(description, studentRating, title, creationTime, replies, ownerID, classID, universityID));
-        }
-
-        if(existsTopics==1) {
-            //todo: while loop to go through all of the questions
-
-        }
-
         return t;
     }
     //Given a topic, it will push this question to the database
