19453f047c202fddf25fd388e1ad02e899967a9e
mr-scneider <rssnyder@iastate.edu>
2017-10-08 14:43:39 -0500

    Working on edorsing and flaging

--- teacher/posts.php
+++ teacher/posts.php
@@ -58,6 +58,11 @@
       echo '<p>' . $text . '</p>  -' . $author . ' @ ' . $creation . '<br><br>';
       //Print the buttons
       echo '<button class="commentButton" onclick="unhide(this,\'childComment' . $id . '\')">Reply</button><a href="utilities/comment.php?class=' . $_GET['class'] . '&thread=' . $threadID . '&comment=' . $id . '&action=flag" class="commentButton">Flag</a><a href="utilities/comment.php?class=' . $_GET['class'] . '&thread=' . $threadID . '&comment=' . $id . '&action=endorse" class="commentButton">Endorse</a>';
+      //Print endorsement
+      if($endorsedID) {
+        echo 'Endorsed!';
+      }
+
       //create the hidden comment box.
       echo '<div id="childComment' . $id . '" class="hidden">
             <div class="content3">';
