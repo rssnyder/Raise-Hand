c899c3ad83dd37488faa2f65c1a2f4fbc75b7d08
mr-scneider <rssnyder@iastate.edu>
2017-10-09 13:23:54 -0500

    forgot a semi-colon

--- admin/pages.php
+++ admin/pages.php
@@ -107,7 +107,7 @@
           //Get all the flagged posts
           $query = 'SELECT * FROM replies WHERE flagged = 1';
           $result = $db->query($query) or die($db->error);
-          echo '<table style="width:100%">'
+          echo '<table style="width:100%">';
           while($omment = $result->fetch_assoc()) {
             echo' <tr>
                <th>' . $comment['user_name'] . '</th>
