5ce34ec4c04ada5533122938524ca354e3865288
Joel Schwartz <joel2@iastate.edu>
2017-10-08 22:28:59 -0500

    Trying to get the classes from teacher/home.php, but Volley won't go into the webpage.

--- RaiseHandAndroid/app/src/main/java/utils/URLS.java
+++ RaiseHandAndroid/app/src/main/java/utils/URLS.java
@@ -8,4 +8,5 @@ public class URLS {
     private static final String ROOT_URL = "http://proj-309-sa-b-3.cs.iastate.edu/";
     public static final String URL_REGISTER = ROOT_URL + "apiSign.php";
     public static final String URL_STRING_LOGIN= ROOT_URL+"api.php";
+    public static final String URL_TEACHER_HOME = ROOT_URL + "teacher/home.php";
 }
