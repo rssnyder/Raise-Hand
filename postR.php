22d94723b858f80f4020d3abfe195a7b4a07d1f9
sae1 <sae1@iastate.edu>
2017-10-11 16:43:54 -0500

    Update postR.php final debug

--- postR.php
+++ postR.php
@@ -13,7 +13,7 @@
 	//start to read from the url
 	//all spaces are encoded as "+"
 	$txt=$_GET['txt'];
-	$txt=str_replace("+"," ", $description);
+	$txt=str_replace("+"," ", $txt);
 	$ownerID= $_GET['OID'];
     $threadID=$_GET['TID'];
     $user_name= $_GET['username'];
