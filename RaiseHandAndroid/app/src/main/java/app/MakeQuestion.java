65d60b303dac84cae81121527423f2aef67ab678
jaggarwal96 <jaggarwal@wisc.edu>
2017-10-11 16:28:58 -0500

    Current user in MakeQuestion

--- RaiseHandAndroid/app/src/main/java/app/MakeQuestion.java
+++ RaiseHandAndroid/app/src/main/java/app/MakeQuestion.java
@@ -1,5 +1,6 @@
 package app;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.sae1.raisehand.R;
+import com.google.gson.Gson;
 
 import utils.Question;
 import utils.URLS;
+import utils.User;
 
 public class MakeQuestion extends AppCompatActivity {
 
     private Button submit;
     EditText textQuestion, titleQuestion;
+    private SharedPreferences mPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +33,22 @@ public class MakeQuestion extends AppCompatActivity {
         final String inputTitle = titleQuestion.getText().toString(); //question title
         final String inputDetails = textQuestion.getText().toString(); //question details
 
+        mPreferences = getSharedPreferences("preferences", MODE_PRIVATE);
+
         // Click the submit button
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Question temp=new Question();
+
+                Gson gson = new Gson();
+                String json = mPreferences.getString("currentUser", "");
+                User currentUser = gson.fromJson(json, User.class);
+
                 //current user's id
-                temp.setOwnerID();
+                temp.setOwnerID(currentUser.getId());
                 //current username
-                temp.setQuestionUsername();
+                temp.setQuestionUsername(currentUser.getUsername());
                 //topic this should fall under
                 temp.setParent();
                 temp.setQuestionTitle(inputTitle);
