30effa89a3799d9c98a09c9f13831ce06fd7bf36
sae1 <sae1@iastate.edu>
2017-10-14 20:03:19 -0500

    Update try.php

--- try.php
+++ try.php
@@ -1,5 +1,7 @@
 <?php
-    //require_once 'sql.php';
+<?php
+
+	//require_once 'sql.php';
 	$host="mysql.cs.iastate.edu";
 	$port=3306;
 	$socket="";
@@ -8,25 +10,70 @@
 	$dbname="db309sab3";
 	//Connect to database
 	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
-	$studentID=$_GET['studentId'];
-	$q = "SELECT * FROM users WHERE ID='$studentID'";
-	$res = $db->query($q) or die($db->error);
-	$question="SELECT * FROM userClasses WHERE user_id=$studentID'";
-	$r = $db->query($question) or die($db->error);
-    //Fetch all of the topics for a given class
-    while($row = $r->fetch_array())
-    {
-        $que="SELECT * FROM topics WHERE class_id='$row[class_id]'";
-        $resu=$db->query($que) or die($db->error);
-        
-        Echo 'NEWTOPIC ';
-        Echo 'CREATETIME ';
-        Echo ''.$row['creation_time'].' ';
-        Echo 'TOPICNAME ';
-        Echo ''.$row['topic_name'].' ';
-        Echo 'ID ';
-        Echo ''.$row['ID'].' ';
-        Echo 'DESCRIPTION ';
-        Echo ''.$row['description'].' ';
-    }
+	$username = $_GET['username'];
+	$pass = $_GET['pass'];
+	$stmt = "SELECT * FROM users WHERE username = '$username'";
+	$stmt = $db->query($stmt) or die($db->error);
+	$response = $stmt->fetch_assoc();
+	$password = $response['pass'];
+
+	//If correct password
+	if(password_verify($pass, $password)) {
+		Echo '{"logged_in": true';
+		Echo ', "reset":';
+		Echo ' '.$response['reset'].' ';
+		Echo ',"id":';
+		Echo ' '.$response['ID'].' ';
+		Echo ', "role_id":';
+	    Echo ' '.$response['role_id'].' ';
+	    Echo ',"username":';
+	    Echo ' '.$response['username'].' ';
+	    Echo ', "first_name":';
+	    Echo ' '.$response['first_name'].' ';
+	    Echo ', "last_name":';
+	    Echo ' '.$response['last_name'].' ';
+	    Echo ', "class_id":';
+		$response['error'] = false;
+		$response['message'] = 'Login successfull';
+		$tempID= $response['ID'];
+	    $stmt = "SELECT * FROM userClasses WHERE user_id = '$tempID' ";
+	    $stmt = $db->query($stmt) or die($db->error);
+	    if(mysqli_num_rows($stmt)>0){
+	        Echo ' ';
+	        
+	        while($row = $stmt->fetch_array()){
+	             if(mysqli_num_rows($stmt)==1){
+	                Echo ' '.$row['class_id'].'';
+                  
+	            }
+	            else{
+	                Echo ''.$row['class_id'].', ';
+	                $que="SELECT * FROM topics WHERE class_id='$row[class_id]'";
+                    $resu=$db->query($que) or die($db->error);
+                     while($roww = $resu->fetch_array()){
+                    Echo 'NEWTOPIC ';
+                    Echo 'CREATETIME ';
+                    Echo ''.$roww['creation_time'].' ';
+                    Echo 'TOPICNAME ';
+                    Echo ''.$roww['topic_name'].' ';
+                    Echo 'ID ';
+                    Echo ''.$roww['ID'].' ';
+                    Echo 'DESCRIPTION ';
+                    Echo ''.$roww['description'].' ';
+                     }
+	            }
+	        }
+	    }
+	   
+	    else{
+	        Echo ' '.'0';
+	    }
+	}
+	else{
+	    Echo '"logged_in ": false';
+		$response['error'] = false;
+		$response['message'] = 'Invalid username or password';
+		$res['class_id']="0";
+	}
+
 ?>
