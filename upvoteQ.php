f7d18b733452c577cb1f5a2bb3b0ce94ba7d5f29
sae1 <sae1@iastate.edu>
2017-10-18 15:31:38 -0500

    Add new file to up vote a question

---
+++ upvoteQ.php
@@ -0,0 +1,15 @@
+<?php
+    //require_once 'sql.php';
+	$host="mysql.cs.iastate.edu";
+	$port=3306;
+	$socket="";
+	$user="dbu309sab3";
+	$password="SD0wFGqd";
+	$dbname="db309sab3";
+	//Connect to database
+	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
+	$question_id=$_GET['QID'];
+	$stmt = "UPDATE threads SET points=points+1 WHERE ID='$question_id$'";
+	$stmt = $db->query($stmt) or die($db->error);
+	die("Done");
+?>
