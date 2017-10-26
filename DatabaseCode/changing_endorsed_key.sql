c87043a1e9fa91cfd2f1cc30f0c56c954b9090fc
Stephanie Engelhardt <sae1@iastate.edu>
2017-10-26 15:18:06 -0500

    database changes

--- DatabaseCode/changing_endorsed_key.sql
+++ DatabaseCode/changing_endorsed_key.sql
@@ -2,3 +2,8 @@ alter table replies add column endorsed int;
 alter table replies drop foreign key replies_ibfk_3;
 alter table replies drop index endorsed_user_id;
 alter table replies drop endorsed_user_id;
+
+alter table threads add column endorsed int DEFAULT 0;
+alter table threads drop foreign key threads_ibfk_3;
+alter table threads drop index endorsed_user_id;
+alter table threads drop endorsed_user_id;
