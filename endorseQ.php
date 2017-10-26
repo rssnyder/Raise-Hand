b7b77269318f12806750577e4d2bc3d75242a9e7
sae1 <sae1@iastate.edu>
2017-10-26 15:33:03 -0500

    Update endorseQ.php

--- endorseQ.php
+++ endorseQ.php
@@ -9,7 +9,7 @@
 	//Connect to database
 	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
 	$question_id=$_GET['QID'];
-	$stmt = "UPDATE replies SET endorsed=1 WHERE ID='$question_id'";
+	$stmt = "UPDATE threads SET endorsed=1 WHERE ID='$question_id'";
 	$stmt = $db->query($stmt) or die($db->error);
 	die("Done");
 ?>
