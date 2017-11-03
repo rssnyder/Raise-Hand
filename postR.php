008cb5931927680caadc87a7bc674a6648dae450
sae1 <sae1@iastate.edu>
2017-11-03 15:16:25 -0500

    Update postR.php

--- postR.php
+++ postR.php
@@ -24,7 +24,7 @@
        $stmt = "INSERT INTO replies(thread_id, owner_id, txt, user_name, parent) VALUES ('$threadID', '$ownerID', '$txt', '$user_name', 0)";
     }
     else{
-        $thread = "SELECT DISTINCT thread_id FROM replies WHERE parent='$parent'";
+        $thread = "SELECT DISTINCT thread_id FROM replies WHERE ID='$parent'";
         $result = $db->query($thread) or die($db->error);
 	    $stmt = "INSERT INTO replies(thread_id, owner_id, txt, user_name, parent) VALUES ('$result', '$ownerID', '$txt', '$user_name', '$parent')";
     }
