65117c924135e1e2414f7925445a5c0009281d65
mr-scneider <rssnyder@iastate.edu>
2017-11-07 10:16:47 -0600

    Correcting live session ending

--- student/liveSession.php
+++ student/liveSession.php
@@ -44,7 +44,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    
+
     <script>
       //Keep track of questions posted to the page
       var feed = 0;
@@ -92,7 +92,8 @@
                    }
                  },
                  error: function() {
-                   alert("Error.");
+                   alert("This live session has ended.");
+                   window.location.href = "pages.php?class=" + $_GET['class'];
                  }
              });
 
