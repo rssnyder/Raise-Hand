c8abea2680a4179a51a07a9207d48e83fe1e175f
Joel Schwartz <joel2@iastate.edu>
2017-11-10 15:42:58 -0600

    let's get ready to make a live feed

---
+++ RaiseHandAndroid/app/src/main/java/Activities/LiveFeed.java
@@ -0,0 +1,15 @@
+package Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sae1.raisehand.R;
+
+public class LiveFeed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_feed);
+    }
+}
