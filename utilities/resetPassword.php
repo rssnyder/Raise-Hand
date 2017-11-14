a849b45e3e0a5261d45eec0511e2a133082d60fd
mr-scneider <rssnyder@iastate.edu>
2017-11-14 10:14:50 -0600

    Further turning things into functions. Cleaning up old files.

--- utilities/resetPassword.php
+++ utilities/resetPassword.php
@@ -1,18 +1,11 @@
 <?php
+include 'database.php';
+
   session_start();
 
-  //TODO Grab all this from a file
-  //Define sql database information
-  $host="mysql.cs.iastate.edu";
-  $port=3306;
-  $socket="";
-  $user="dbu309sab3";
-  $password="SD0wFGqd";
-  $dbname="db309sab3";
   //Connect to database
-  $db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
-  //Print out host information
-  //echo $db->host_info;
+  $db = getDB();
+
   //Make sure everything is there and then reset password
   if("" == trim($_POST['password1'])) {
     $_SESSION['error'] = true;
--- utilities/signin.php
+++ utilities/signin.php
@@ -1,16 +1,11 @@
 <?php
+include 'database.php';
+
   //Start a session
   session_start();
-  //Define paramiters and initalize connection
-  $host="mysql.cs.iastate.edu";
-	$port=3306;
-	$socket="";
-	$user="dbu309sab3";
-	$password="SD0wFGqd";
-	$dbname="db309sab3";
-  $db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
-  //Print out host information
-  echo $db->host_info;
+
+  //Connect to database
+  $db = getDB();
 
   //Check and make sure they entered a username
   if("" == trim($_POST['username'])) {
