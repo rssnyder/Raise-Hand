a954a7efa38d8588bffb31ed8ecfb29f94fa9666
sae1 <sae1@iastate.edu>
2017-11-27 20:19:10 -0600

    Update notifications.php

--- android/notifications.php
+++ android/notifications.php
@@ -9,7 +9,7 @@
 	//Connect to database
 	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
 	$class_id=$_GET['classId'];
-    $userClasses= "SELECT ID FROM topics WHERE class_id IN ({implode('+', $class_id)})";
+    $userClasses= "SELECT ID FROM topics WHERE class_id IN ({explode('+', $class_id)})";
     Echo $userClasses;
     $topic=$db->query($userClasses) or die($db->error);
     $topics=$topic->fetch_array();
