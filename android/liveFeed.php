d60b08a9ec27dc8959a71a0ee193187b13ad1b50
joel2 <joel2@iastate.edu>
2017-11-27 21:46:36 -0600

    Update liveFeed.php

--- android/liveFeed.php
+++ android/liveFeed.php
@@ -13,7 +13,6 @@
   	$result = $db->query($query) or die($db->error);
 
   	while($class = $result->fetch_assoc()) {
-	  	Echo '{"live_session": true';
 	  	Echo ', "ID":';
 	  	Echo ' '.$class['ID'].' ';
 	  	Echo ', "username":';
