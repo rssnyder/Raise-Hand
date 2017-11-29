3a680cc7a23da8e437cc3b4e4d6a8ab8fc27c515
sae1 <sae1@iastate.edu>
2017-11-29 16:16:05 -0600

    Update androidApi.php

--- android/androidApi.php
+++ android/androidApi.php
@@ -4,58 +4,90 @@
     
   */
 
-   /// Start a connection to the project database.
-   /// @return A database referance variable.
-  function getDB() {
-    return $db;
-  }
+    /// Start a connection to the project database.
+    /// @return A database referance variable.
+    function getDB() {
+        return $db;
+    }
 
-  /// Get all the information on a given class.
-  /// @param $db the database to query.
-  /// @param $class the class ID in integer or string form.
-  /// @return Array of class properties.
-  function getClass($db, $class) {
-    //Get this class
-    $query = "SELECT * FROM classes WHERE ID = " . $class;
-    $result = $db->query($query) or die($db->error);
-    return $result->fetch_assoc();
-  }
-
-  /// Get all the class information for classes that a user belongs to.
-  /// @param $db the database to query.
-  /// @param $qID the id of the question to endorse
-  /// @return A database result that is a list of class information arrays.
-  function endorseQ($db, $qID) {
-    return $result;
-  }
+    /// Get all the class information for classes that a user belongs to.
+    /// @param $db the database to query.
+    /// @param $qID the id of the question to endorse
+    /// @return A database result that is a list of class information arrays.
+    function endorseQ($db, $qID) {
+        return $result;
+    }
   
-  /// Get all the class information for classes that a user belongs to.
-  /// @param $db the database to query.
-  /// @param $rID the id of the reply to endorse
-  /// @return A database result that is a list of class information arrays.
-  function endorseR($db, $rID) {
-    return $result;
-  }
+    /// Get all the class information for classes that a user belongs to.
+    /// @param $db the database to query.
+    /// @param $rID the id of the reply to endorse
+    /// @return A database result that is a list of class information arrays.
+    function endorseR($db, $rID) {
+        return $result;
+    }
   
   
-  function forgotPassword($db, $email){
-      return $result;
-  }
+    function forgotPassword($db, $email){
+        return $result;
+    }
 
-function liveFeed($db, $classId){
-    return $result;
-}
+    function liveFeed($db, $classId){
+        return $result;
+    }
 
-function login($db, $username, $password){
-    
-}
+    function login($db, $username, $password){
+        return $result;
+    }
 
-function notifications($db, $classList){
-    
-}
+    function notifications($db, $classList){
+        return $result;
+    }
 
-function postQ($db, $description, $title, $ownerId, $topicId, $username){
-    
-}
+    function postQ($db, $description, $title, $ownerId, $topicId, $username){
+        return $result;
+    }
 
- ?>
+    function postR($db, $txt, $username, $ownerId, $threadId, $replyParentId){
+        return $result;
+    }
+
+    function refreshQ(){
+        return $result;
+    }
+
+    function refreshR(){
+        return $result;
+    }
+
+    function refreshRR(){
+        return $result;
+    }
+
+    function reportQ(){
+        return $result;
+    }
+
+    function reportR(){
+        return $result;
+    }
+
+    function resetPassword(){
+        return $result;
+    }
+
+    function signup(){
+        return $result;
+    }
+
+    function topics(){
+        return $result;
+    }
+
+    function upvoteQ(){
+        return $result;
+    }
+
+    function upvoteR(){
+        return $result; 
+    }
+?>
