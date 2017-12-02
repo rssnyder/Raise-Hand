3e4f28330bbe28b391c055145bee2e792fcc4507
sae1 <sae1@iastate.edu>
2017-12-02 12:17:58 -0600

    Update notifications.php

--- android/notifications.php
+++ android/notifications.php
@@ -10,14 +10,10 @@
 	$db = new mysqli($host, $user, $password, $dbname, $port, $socket) or die ('Could not connect to the database server' . mysqli_connect_error());
 	$class_id=$_GET['classId'];
 	$classes=explode('+', $class_id);
-	foreach($classes as $classID){
-	    echo $classID;
-	}
 	$list='(';
-	foreach ($classes as $item) {
-        $list+= $item . ', ';
-        echo $item;
-    }
+	foreach($classes as $classID){
+	   $list.=$classID . ', ';
+	}
     echo $list;
     $list=substr($list, 0, strlen($list)-2);
     $list+=")";
