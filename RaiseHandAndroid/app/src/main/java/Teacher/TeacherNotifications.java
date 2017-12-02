ba09c59a301f874436a6ed633cc00d027bfb1684
Stephanie Engelhardt <sae1@iastate.edu>
2017-12-02 14:14:25 -0600

    ugh

--- RaiseHandAndroid/app/src/main/java/Teacher/TeacherNotifications.java
+++ RaiseHandAndroid/app/src/main/java/Teacher/TeacherNotifications.java
@@ -1,5 +1,7 @@
 package Teacher;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -9,8 +11,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
 import com.example.sae1.raisehand.R;
 import com.google.gson.Gson;
 
@@ -19,14 +27,19 @@ import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import Activities.VolleyMainActivityHandler;
 import RecyclerViews.ListItemTeacherNotifications;
 import RecyclerViews.MyAdapterNotifications;
 import RecyclerViews.MyAdapterQuestions;
 import Utilities.ActivitiesNames;
+import Utilities.Classes;
 import Utilities.NavUtil;
 import Utilities.Question;
 import Utilities.RecentActivity;
+import Utilities.StringParse;
+import Utilities.URLS;
 import Utilities.User;
 
 /**
@@ -35,8 +48,9 @@ import Utilities.User;
  * Notifications will include recent questions and replies submitted.
  * @author sae1
  */
-public class TeacherNotifications extends AppCompatActivity {
-
+public class TeacherNotifications extends Activity {
+    private static String TAG= TeacherNotifications.class.getSimpleName();
+    private static String tag_string_req= "string_req";
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<Question> listItems;
@@ -47,6 +61,7 @@ public class TeacherNotifications extends AppCompatActivity {
     private ActionBarDrawerToggle mToggle;
     private Toolbar mToolbar;
     private NavigationView nv;
+    private ProgressDialog pDialog;
     private Field mDragger;
     /**
      *
@@ -60,13 +75,14 @@ public class TeacherNotifications extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_questions);
         mPreferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        pDialog= new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
+
         Gson gson = new Gson();
         String json = mPreferences.getString("currentUser", "");
         currentUser = gson.fromJson(json, User.class);
-
-        RecentActivity r=new RecentActivity();
-        listItems =  r.getNotifications(currentUser.getClasses());
-
+        getNotifications(currentUser.getClasses());
         // Adapter to display the questions as recycler views. (cards on the screen)
         adapter = new MyAdapterQuestions(listItems,this);
         // Setting up the recycler view
@@ -77,7 +93,7 @@ public class TeacherNotifications extends AppCompatActivity {
 
         // Get the nav menu
         mToolbar = (Toolbar) findViewById(R.id.nav_action);
-        setSupportActionBar(mToolbar);
+        //setSupportActionBar(mToolbar);
 
         // create the drawer layout (the thing you swipe from the side)
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
@@ -88,7 +104,7 @@ public class TeacherNotifications extends AppCompatActivity {
         mToggle.syncState();
 
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // populate the navigation buttons to go to the correct place
         nv = (NavigationView) findViewById(R.id.nv1);
@@ -109,5 +125,69 @@ public class TeacherNotifications extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void showProgressDialog() {
+        if(!pDialog.isShowing()) {
+            pDialog.show();
+        }
+    }
+
+    private void hideProgressDialog() {
+        if(pDialog.isShowing()) {
+            pDialog.hide();
+            pDialog.dismiss();
+        }
+    }
+    /**
+     * This method will return an array list of questions that were recently posted in
+     * his or her classes. It will return an empty array list if he or she is not in any classes
+     * or if there is no recent activity
+     * @param userClasses the classes the user is currently in
+     * @return an array list of questions that were recently posted in his or her classes
+     */
+    private void getNotifications(ArrayList<Classes> userClasses){
+        showProgressDialog();
+        listItems=new ArrayList<Question>();
+        String urlSuffix="";
+        if(!userClasses.isEmpty()) {
+            urlSuffix = "?classId=" + userClasses.get(0).getClassID();
+        }
+        for(int i=1; i<userClasses.size()-1; i++) {
+            urlSuffix+=","+userClasses.get(i).getClassID();
+
+        }
+        if(urlSuffix.isEmpty()){
+            //this means the student is not in any classes
+
+            return;
+        }
+        String url_final = URLS.URL_NOTIFICATIONS + urlSuffix;
+        System.out.println(url_final);
+
+        StringRequest req = new StringRequest(Request.Method.GET,url_final,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, response.toString());
+                        String phpResponse=response.toString();
+                        listItems= StringParse.parseQuestions(phpResponse);
+                        SharedPreferences.Editor editor = mPreferences.edit();
+                        Gson gson = new Gson();
+                        String json = gson.toJson(listItems);
+                        editor.putString("listItems", json);
+                        editor.commit();
+                        hideProgressDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+            }
+        }
+        );
+        // Adding request to request queue
+        VolleyMainActivityHandler.getInstance().addToRequestQueue(req, tag_string_req);
+        System.out.println(listItems.toString());
     }
 }
