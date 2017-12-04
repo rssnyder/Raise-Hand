e292fe0be76aaff2bcfe3779375e5a8fb5189003
Stephanie Engelhardt <sae1@iastate.edu>
2017-12-04 10:45:24 -0600

    fixed the null class on the bottom of the class list

--- RaiseHandAndroid/app/src/main/java/Teacher/TeacherClasses.java
+++ RaiseHandAndroid/app/src/main/java/Teacher/TeacherClasses.java
@@ -66,7 +66,8 @@ public class TeacherClasses extends AppCompatActivity {
 
         // Get the classes the current user is in
         listItems = currentUser.getClasses();
-
+        //remove the last question because it is null
+        listItems.remove(currentUser.getClasses().get(currentUser.getClasses().size()-1));
         // Adapter to display the classes as recycler views. (cards on the screen)
         adapter = new MyAdapterClasses(listItems, this);
 
