34e396f92b93e2b0f993e8633f98591e5cf47b13
Stephanie Engelhardt <sae1@iastate.edu>
2017-12-04 14:54:46 -0600

    another null pointer changed

--- RaiseHandAndroid/app/src/main/java/Student/StudentNotifications.java
+++ RaiseHandAndroid/app/src/main/java/Student/StudentNotifications.java
@@ -100,7 +100,7 @@ public class StudentNotifications extends AppCompatActivity {
 
         // populate the navigation buttons to go to the correct place
         nv = (NavigationView) findViewById(R.id.nv1);
-        NavUtil.setNavMenu(nv, ActivitiesNames.NONE, getApplicationContext(), mDrawerLayout);
+        NavUtil.setNavMenu(nv, ActivitiesNames.NOTIFICATIONS, getApplicationContext(), mDrawerLayout);
 
     }
 
